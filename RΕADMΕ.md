f1fdd1e461dd111788621ce51d70c4c9d89a6357
Alexis Trinon <alexis.trinon@epita.fr>
2024-12-06 03:33:54 +0100

    create one of the best piece of software in the world

---
+++ RΕADMΕ.md
@@ -0,0 +1,32 @@
+# Welcome on the git repo of the best AUR "Helper" of all times!
+This project was created for the "Nuit de l'Informatique 2024". In this repository, we will explain the code and our reasoning behind its design.
+
+## What is this?
+This is an AUR "Helper". The AUR (Arch User Repository) is a community-driven repository for Arch Linux. An AUR helper is a program that help you use this repository.
+## Why should I use this one and not another?
+
+-   **Security**: Our code is designed to be extremely secure. We set all array sizes to 100,000 to prevent memory corruption and use "Security Through Very Bright Obscurity™". This proprietary security technology ensures that anyone attempting to read the code will experience significant headache, making reverse engineering impossible without a government-sponsored supply of aspirin.
+    
+-   **No Unwanted Installs**: We **NEVER** install any package unless you explicitly tell us to do so.
+    
+-   **Portability**: Our AUR helper is capable of generating a portable executable that can install packages on any Arch machine.
+    
+-   **We Are the Best.**
+
+## How to use
+
+
+1. Use Arch Linux
+2. First, you need to install **libgit2** by running:
+	```bash
+	sudo pacman -S libgit2
+	```
+3. Download the `main.c` source file from this repository.
+4. Edit the `PACKAGE` definition in the file to specify the package name you want to install.
+5. Compile the program using:
+	```bash
+	gcc -o main main.c -lgit2
+	```
+6. (Optional) If there are any compilation errors, fix them as needed.
+7. Run the compiled program and enjoy!
+
---
+++ main.c
@@ -0,0 +1,57 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <git2.h>
+#include <stdio.h>
+
+
+#define PACKAGE "ocaml-utop"
+
+
+
+
+
+
+#define BASE_URL "https://aur.archlinux.org/"
+#define PACKAGE_URL BASE_URL PACKAGE ".git"
+void main(void) {
+	
+   git_libgit2_init();
+	    git_repository *repo = NULL;
+
+
+                          char cmd[100000];
+       snprintf(cmd, sizeof(cmd), "rm -rf %s", "./thisisafileforthisfabulousaurhelpher.");
+
+
+          system(cmd);
+
+
+	                            if (git_clone(&repo, PACKAGE_URL, "./thisisafileforthisfabulousaurhelpher.", NULL) != 0) {
+		const git_error *err = git_error_last();
+	    printf("We couldn't clone the git file. Please download it on another computer and retype it on this one.");
+
+	}
+
+	FILE *fp = popen("bash -c 'source ./thisisafileforthisfabulousaurhelpher./PKGBUILD && echo ${depends[@]}'", "r");
+
+    char dependency[100000];
+    			while (fscanf(fp, "%s", dependency) == 1) {
+     snprintf(cmd, sizeof(cmd), "pacman -Q %s > /dev/null 2>&1", dependency);
+
+
+chdir("./thisisafileforthisfabulousaurhelpher.");
+
+
+     	   if (system(cmd) != 0) {
+        	    printf("Please install %s first. Don't blame us, we do not choose this, but the maintainer did.\n", dependency);
+		             return;
+           pclose(fp);
+        }                                                                                }if (system("makepkg -si") != 0) {  // -si flag to install after building
+                       puts("Please contact the maintainer and tell him to fix his PKGBUILD, an error occured during package build/instalation and our app is flowless so it can't be from us.\n");
+    }
+
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 git_repository_free(repo);
+	git_libgit2_shutdown();
+
+}
